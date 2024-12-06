dc1271f23d45978e727af2487a4aa4a2bf5993f8
최은영 <dev.choiey@gmail.com>
2024-12-06 18:02:07 +0900

    feat: 말동무 채팅 입력값이 없을 시 전송 버튼 비활성화

--- src/pages/ey/EYmain.js
+++ src/pages/ey/EYmain.js
@@ -1,8 +1,17 @@
 // src/pages/ey/EYmain.js
-import React from 'react';
+import React, { useState } from 'react';
 import './EYmain.css'; // 스타일링을 위한 CSS 파일 import
 
 function EYmain() {
+  // 입력 상태 관리
+  const [inputText, setInputText] = useState(''); 
+
+  // 입력값 변경 시 상태 업데이트
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  }
+
+
   return (
     <div className="welcome-container">
       <h1 className="welcome-title">점심은 드셨나요?<br />오늘 드신 점심메뉴를 이야기해주세요!</h1>
@@ -13,8 +22,14 @@ function EYmain() {
           placeholder="오늘 날씨가 참 좋네."
           className="text-input"
           aria-label="대화 입력창"
+          value={inputText} // 입력값 상태 연결
+          onChange={handleInputChange} // 입력값 변경 이벤트 처리
         />
-        <button className="send-button" aria-label="전송 버튼">
+        <button 
+          className="send-button" 
+          aria-label="전송 버튼"
+          disabled={!inputText.trim()} // 입력값이 비어 있으면 비활성화
+          >
           <span className="arrow-icon">➤</span>
         </button>
         <button className="mic-button" aria-label="음소거 버튼">
