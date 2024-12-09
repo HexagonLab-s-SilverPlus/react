e051a9228665400fcee589f233cc15bdb0de36be
최은영 <dev.choiey@gmail.com>
2024-12-09 09:51:58 +0900

    Merge branch 'hs' into eunyoung

--- src/pages/qna/QNAList.js
+++ src/pages/qna/QNAList.js
@@ -1,9 +1,103 @@
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import SideBar from '../../components/common/SideBar';
+import QNAHeader from '../../components/qna/QNAHeader';
+import styles from './QNAList.module.css'
 
 function QNAList() {
-  return <SideBar />;
+
+  const navigate = useNavigate();
+
+  const handleMoveDetailView= () => {
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <SideBar />
+      <div className={styles.qnaContent}>
+        <QNAHeader />
+        <div className={styles.qnaSeachdiv}>
+          <select className={styles.qnaSeachselect}>
+              <option value="title" selected>제목</option>
+              <option value="writer">작성자</option>
+              <option value="date">날짜</option>
+          </select>
+          <input className={styles.qnaInput}></input>
+          <button className={styles.qnaInputBTN}>등 록</button>
+        </div>
+        <table className={styles.qnaListTable}>
+          <tr>
+            <th className={styles.qnaTitleH}>제목</th>
+            <th className={styles.qnaWCreateByH}>작성자</th>
+            <th className={styles.qnaWCreateAtH}>등록날짜</th>
+            <th className={styles.qnaStateH}>상태</th>
+          </tr>
+          <tr>
+            <td ><button onClick={handleMoveDetailView}>아이디를 까먹었어요</button></td>
+            <td className={styles.qnaWCreateBy}>작성자</td>
+            <td className={styles.qnaWCreateAt}>2024/12/15</td>
+            <td className={styles.qnaStateN}>미답변</td>
+          </tr>
+          <tr>
+            <td ><button onClick={handleMoveDetailView}>아이디를 까먹었어요</button></td>
+            <td className={styles.qnaWCreateBy}>작성자</td>
+            <td className={styles.qnaWCreateAt}>2024/12/15</td>
+            <td className={styles.qnaStateN}>미답변</td>
+          </tr>
+          <tr>
+            <td ><button onClick={handleMoveDetailView}>아이디를 까먹었어요</button></td>
+            <td className={styles.qnaWCreateBy}>작성자</td>
+            <td className={styles.qnaWCreateAt}>2024/12/15</td>
+            <td className={styles.qnaStateN}>미답변</td>
+          </tr>
+          <tr>
+            <td ><button onClick={handleMoveDetailView}>아이디를 까먹었어요</button></td>
+            <td className={styles.qnaWCreateBy}>작성자</td>
+            <td className={styles.qnaWCreateAt}>2024/12/15</td>
+            <td className={styles.qnaStateN}>미답변</td>
+          </tr>
+          <tr>
+            <td ><button onClick={handleMoveDetailView}>아이디를 까먹었어요</button></td>
+            <td className={styles.qnaWCreateBy}>작성자</td>
+            <td className={styles.qnaWCreateAt}>2024/12/15</td>
+            <td className={styles.qnaStateY}>답변완료</td>
+          </tr>
+          <tr>
+            <td ><button onClick={handleMoveDetailView}>아이디를 까먹었어요</button></td>
+            <td className={styles.qnaWCreateBy}>작성자</td>
+            <td className={styles.qnaWCreateAt}>2024/12/15</td>
+            <td className={styles.qnaStateY}>답변완료</td>
+          </tr>
+          <tr>
+            <td ><button onClick={handleMoveDetailView}>아이디를 까먹었어요</button></td>
+            <td className={styles.qnaWCreateBy}>작성자</td>
+            <td className={styles.qnaWCreateAt}>2024/12/15</td>
+            <td className={styles.qnaStateY}>답변완료</td>
+          </tr>
+          <tr>
+            <td ><button onClick={handleMoveDetailView}>아이디를 까먹었어요</button></td>
+            <td className={styles.qnaWCreateBy}>작성자</td>
+            <td className={styles.qnaWCreateAt}>2024/12/15</td>
+            <td className={styles.qnaStateY}>답변완료</td>
+          </tr>
+          <tr>
+            <td ><button onClick={handleMoveDetailView}>아이디를 까먹었어요</button></td>
+            <td className={styles.qnaWCreateBy}>작성자</td>
+            <td className={styles.qnaWCreateAt}>2024/12/15</td>
+            <td className={styles.qnaStateY}>답변완료</td>
+          </tr>
+          <tr>
+            <td ><button onClick={handleMoveDetailView}>아이디를 까먹었어요</button></td>
+            <td className={styles.qnaWCreateBy}>작성자</td>
+            <td className={styles.qnaWCreateAt}>2024/12/15</td>
+            <td className={styles.qnaStateY}>답변완료</td>
+          </tr>
+          
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default QNAList;
