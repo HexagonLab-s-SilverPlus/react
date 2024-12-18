b2039ffcacc29956a89b4b1ddb9721db9e344d4b
ghdtpwlq <84121339+ghdtpwlq@users.noreply.github.com>
2024-12-18 10:12:12 +0900

    feat: qna insert

--- src/routers/qnaRouter.js
+++ src/routers/qnaRouter.js
@@ -1,10 +1,12 @@
 // src/routers/noticeRouter.js
 import React from 'react';
 import { Route } from 'react-router-dom';
-import QNAList from '../pages/qna/QNAList';
+import QnAList from '../pages/qna/QnAList';
+import QnAWrtie from '../pages/qna/QnAWrite'
 
 const qnaRouter = [
-    <Route path="/qna" element={<QNAList />} />,
+    <Route path="/qna" element={<QnAList />} />,
+    <Route path="/qna/write" element={<QnAWrtie />} />,
 ];
 
 export default qnaRouter;
