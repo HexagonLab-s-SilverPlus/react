b89f1c0e51451624d29f10450fc9f0520a7b6aa8
TJ-kim0514 <ktj338920@gmail.com>
2024-12-19 16:32:11 +0900

    refactor: axios url 명칭수정

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, // .env 파일에 설정된 URL 사용
+  baseURL: process.env.REACT_APP_SPRING_BOOT_API_URL, // .env 파일에 설정된 URL 사용
 });
 
 export default apiClient;
