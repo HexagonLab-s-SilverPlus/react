526bc8eda03b20c53a3f2248b177e963ffb14811
sujannoh <nohsoojin6@gmail.com>
2024-12-19 16:06:03 +0900

    대시보드 List 틀 잡기
    대시보드 List 초기 틀 잡기

--- src/routers/sjRouter.js
+++ src/routers/sjRouter.js
@@ -2,18 +2,18 @@
 // src/routers/tjRouter.js
 import React from 'react';
 import { Route } from 'react-router-dom';
-import SJmain from '../pages/sj/SJmain';
+
 import DocMain from '../pages/sj/DocMain';
-import DashMain from '../pages/sj/DashMain';
+import DashList from '../pages/sj/DashList';
 
 
 
 
 
 const sjRouter = [
-    <Route path="/sjmain" element={<SJmain />} />,
+    
     <Route path="/docmain" element={<DocMain/>}/>,
-    <Route path="/dashmain" element={<DashMain/>}/>,
+    <Route path="/dashlist" element={<DashList/>}/>,
     
 
 
