c646637981696414fd6d9abffb8b8c656c19c2f2
최은영 <dev.choiey@gmail.com>
2024-12-19 16:11:54 +0900

    feat: pull 전에 커밋

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,5 +3,5 @@
 import axios from 'axios';
 
 export const apiSpringBoot = axios.create({
-  baseURL: process.env.REACT_APP_SPRING_BOOT_API_URL// .env 파일에 설정된 URL 사용
+  baseURL: process.env.REACT_APP_SPRING_BOOT_API_URL
 });
