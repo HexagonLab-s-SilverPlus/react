4898574247a4a8ea373fba3dad041386b068f72c
ghdtpwlq <84121339+ghdtpwlq@users.noreply.github.com>
2024-12-23 15:04:22 +0900

    feat
    페이징 처리

--- src/components/common/Paging.js
+++ src/components/common/Paging.js
@@ -4,14 +4,16 @@ import styles from './Paging.module.css';
 
 
 const Paging = ({ 
-  currentPage, 
+  pageNumber, 
+  listCount,
+  pageSize,
   maxPage, 
   startPage,
   endPage,
   onPageChange
   }) => {
     const groupSize = 10;  //페이지 그룹 크기 (한 그룹에 페이지 숫자 10개)
-    const currentGroup = Math.ceil(currentPage / groupSize);  //현재 페이지가 속한 페이지그룹
+    const currentGroup = Math.ceil(pageNumber / groupSize);  //현재 페이지가 속한 페이지그룹
     //만약, 페이지그룹의 시작과 끝숫자를 서버에서 받지 않는다면(현재페이지, 총페이지수만 받은 경우) 직접 계산함 
     //const startPage = (currentGroup - 1) * groupSize + 1; //현재 그룹의 시작 페이지
     //const endPage = Math.min(currentGroup * groupSize, maxPage);  // 현재 그룹의 끝페이지
@@ -26,7 +28,7 @@ const Paging = ({
       <div className={styles.pagingContainer}>
         {/* 1페이지로 이동 버튼 */}
         <button
-          disabled={currentPage === 1}
+          disabled={pageNumber === 1}
           onClick={() => onPageChange(1)}>&lt; &lt;</button>
         {/* 이전 페이지 그룹 이동 버튼 */}
         <button
@@ -36,7 +38,7 @@ const Paging = ({
         {pages.map((page) => (
           <button
             key={page}
-            className={currentPage === page ? styles.activePage : ''}
+            className={pageNumber === page ? styles.activePage : ''}
             onClick={() => onPageChange(page)}>{page}</button>
         ))}
         {/* 다음 페이지 그룹 이동 버튼 */}
@@ -45,7 +47,7 @@ const Paging = ({
           onClick={() => onPageChange(endPage + 1)}>&gt;</button>
         {/* 끝페이지로 이동 버튼 */}
         <button
-          disabled={currentPage === maxPage}
+          disabled={pageNumber === maxPage}
           onClick={() => onPageChange(maxPage)}>&gt;&gt;</button>
       </div>
     );
