4072a9741511691d780aa683826022cc26e70a59
sujannoh <nohsoojin6@gmail.com>
2024-12-24 12:43:56 +0900

    feat. 대시보드 캘린더 구현 및 todo insert 작성중입니다.
    대시보드 캘린더 구현 및 todo insert 작성중입니다.

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,7 +4,8 @@ import { Routes, Route } from 'react-router-dom';
 // 통합하기 위해 별도로 작성된 라우터 파일들을 임포트함
 import memberRouter from './memberRouter';
 import noticeRouter from './noticeRouter';
-import sjRouter from './sjRouter';
+import dashboardRouter from './dashboardRouter';
+import documentRouter from './documentRouter';
 import siRouter from './siRouter';
 import eyRouter from './eyRouter';
 import qnaRouter from './qnaRouter';
@@ -19,7 +20,8 @@ const AppRouter = () => {
       <Route path="/" element={<WelcomePage />} />
       {memberRouter}
       {noticeRouter}
-      {sjRouter}
+      {dashboardRouter}
+      {documentRouter}
       {siRouter}
       {eyRouter}
       {qnaRouter}
