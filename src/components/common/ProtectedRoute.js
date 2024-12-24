c26c688a8a8e9723c1daa1942857caa1aae59516
최은영 <dev.choiey@gmail.com>
2024-12-24 14:30:15 +0900

    Update ProtectedRoute.js

--- src/components/common/ProtectedRoute.js
+++ src/components/common/ProtectedRoute.js
@@ -4,4 +4,15 @@
 //* 인증되지 않은 사용자는 로그인 페이지로 리다이렉트된다.
 
 import React, { useContext } from 'react';
-import { Navigate } form ''
+import { Navigate } from 'react-router-dom';
+// AuthContext를 import 하여 인증 상태에 접근
+import { AuthContext } from '../../AuthProvider';
+
+// ProtectedRoute 컴포넌트 정의
+const ProtectedRoute = ({ children, roles = [] }) => {
+    // AuthContext에서 auth 상태를 가져옴
+    const { authInfo } = useContext(AuthContext);
+
+    // 인증되지 않은 사용자인 경우 (accessToken이 없을 때)
+    if()
+}
