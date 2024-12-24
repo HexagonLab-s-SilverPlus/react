c8ad295438e6d2840618d3d485c01da6225c4e07
최은영 <dev.choiey@gmail.com>
2024-12-24 15:03:02 +0900

    feat: ProtectedRouter 구현
    - 인증된 사용자만 보호된 라우트에 접근 가능토록 함
    - `...authInfo`를 AuthProvider에 넘겼으니 그 안에 있는 속성들을 글로벌하게 바로 접근가능함
        - 예를 들면, `const { accessToken, role} = useContext(AuthContext);`

--- src/components/common/ProtectedRoute.js
+++ src/components/common/ProtectedRoute.js
@@ -1,18 +1,34 @@
-// src/components/common/ProtectedRoute.js
-//* 인증된 사용자만 접근할 수 있도록 하기 위함이다.
-//* 이 컴포넌트를 사용하여 보호된 페이지에 접근할 때 사용자의 인증 상태를 확인하고,
-//* 인증되지 않은 사용자는 로그인 페이지로 리다이렉트된다.
-
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { Navigate } from 'react-router-dom';
 // AuthContext를 import 하여 인증 상태에 접근
 import { AuthContext } from '../../AuthProvider';
 
 // ProtectedRoute 컴포넌트 정의
-const ProtectedRoute = ({ children, roles = [] }) => {
+const ProtectedRoute = ({ element, roles = [], }) => {
     // AuthContext에서 auth 상태를 가져옴
-    const { authInfo } = useContext(AuthContext);
+    const { accessToken, role } = useContext(AuthContext);
 
     // 인증되지 않은 사용자인 경우 (accessToken이 없을 때)
-    if()
-}
+    if (!accessToken) {
+        return <Navigate to="/loginmember" replace />;
+    }
+
+    // 역할 기반 접근 제어
+    if (roles.length > 0 && !roles.includes(role)) {
+        return <Navigate to="/" />;
+    }
+
+    // 인증된 사용자인 경우, 자식 컴포넌트를 렌더링
+    return element;
+};
+
+// PropTypes 설정
+// ESLint가 PropTypes 검사를 설정하지 않아 경고가 뜨길래 아래 코드로 선언해서 해결함
+ProtectedRoute.propTypes = {
+    element: PropTypes.node.isRequired, // children은 React 노드로 설정하며 필수 값이다.
+    roles: PropTypes.arrayOf(PropTypes.string), // roles는 문자열 배열이며, 선택적(Optional), 기본값은 빈 배열이다.
+};
+
+// ProtectedRoute 컴포넌트를 export 하여, 다른 컴포넌트에서 사용할 수 있게 함
+export default ProtectedRoute;
