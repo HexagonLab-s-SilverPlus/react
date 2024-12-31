44e354e71dd9f9d0068cee44e4d1e84d62bdc91f
SangMoo Lee <dltkdan7373@hanmail.net>
2024-12-31 18:17:58 +0900

    faet. 페이지이동 stt구현중

--- src/components/common/PageRider.js
+++ src/components/common/PageRider.js
@@ -1,12 +1,28 @@
-// /src/components/common/PageRider.js
-import React from "react";
+import React, { useState } from "react";
 import styles from "./PageRider.module.css";
-import button from "../../assets/images/chatIcon.png"
+import button from "../../assets/images/chatIcon.png";
+import SpeechRecognitionModal from "./SpeechRecognitionModal";
 
-const PageRider = (onClick) => {
+const PageRider = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleButtonClick = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <>
-            <img src={button} className={styles.floatingButton} onClick={onClick}></img>
+            <img
+                src={button}
+                className={styles.floatingButton}
+                onClick={handleButtonClick}
+                alt="Floating Button"
+            />
+            {isModalOpen && <SpeechRecognitionModal onClose={closeModal} />}
         </>
     );
 };
