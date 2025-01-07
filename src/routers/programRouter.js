af2cb3839c9c717aa258c92c1f39e8057fddfad8
hanadoolyseoi <hanadoolyseoi@gmail.com>
2025-01-07 22:30:56 +0900

    feat: 병력관리 추가 구현 완료
    feat: 병력관리 추가 구현 완료

--- src/routers/programRouter.js
+++ src/routers/programRouter.js
@@ -5,13 +5,15 @@ import ProgramList from '../pages/program/ProgramList';
 import ProgramWrite from '../pages/program/ProgramWrite';
 import ProgramDetail from '../pages/program/ProgramDetail';
 import ProgramUpdate from '../pages/program/ProgramUpdate';
+import TestMedical from '../pages/program/TestMedical';
 
 const programRouter = [
     <>
-    <Route path="/program" element={<ProgramList />} />,
-    <Route path="/program/write" element={<ProgramWrite />} />,
-    <Route path="/program/detail/:snrProgramId" element={<ProgramDetail />} />,
-    <Route path="/program/update/:snrProgramId" element={<ProgramUpdate />} />,
+        <Route path="/program" element={<ProgramList />} />,
+        <Route path="/program/write" element={<ProgramWrite />} />,
+        <Route path="/program/detail/:snrProgramId" element={<ProgramDetail />} />,
+        <Route path="/program/update/:snrProgramId" element={<ProgramUpdate />} />,
+        <Route path="/program/medical" element={<TestMedical />} />,
     </>
 ];
 
