17a0a6463cee8ab17489b3d43e942894381ebc00
최은영 <dev.choiey@gmail.com>
2025-01-09 17:01:26 +0900

    style: NotFound 화면 꾸밈

--- src/pages/common/NotFound.js
+++ src/pages/common/NotFound.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Player } from '@lottiefiles/react-lottie-player';
+import styles from './NotFound.module.css';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 - 페이지를 찾을 수 없습니다.</h1>
-      <p>잘못된 URL입니다. 다시 시도해주세요.</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>404 - 페이지를 찾을 수 없습니다.</h1>
+      <p className={styles.description}>잘못된 URL입니다. 다시 시도해주세요.</p>
+      <Player
+        autoplay
+        loop
+        src="/lottie/doc-loading-anime2.json" // Lottie 파일 경로
+        className={styles.lottie}
+      />
     </div>
   );
 };
