c9f6faf8a0d912f6af2957c84c22dd3ff0dc109a
SangMoo Lee <dltkdan7373@hanmail.net>
2025-01-09 10:08:15 +0900

    feat. protect

--- src/routers/noticeRouter.js
+++ src/routers/noticeRouter.js
@@ -5,14 +5,14 @@ import NoticeList from '../pages/notice/NoticeList'; // 공지사항 목록
 import NoticeWrite from '../pages/notice/NoticeWrite'; // 공지사항 쓰기기
 import NoticeDetail from '../pages/notice/NoticeDetail'; // 공지사항 상세보기
 import NoticeUpdate from '../pages/notice/NoticeUpdate'; // 공지사항 수정
-
+import ProtectedRoute from '../components/common/ProtectedRoute';
 
 const noticeRouter = [
     <>
-    <Route path="/notice" element={<NoticeList />} />,
-    <Route path="/noticewrite" element={<NoticeWrite />} />,
-    <Route path="/noticedetail/:notId" element={<NoticeDetail />} />,
-    <Route path="/noticeupdate/:notId" element={<NoticeUpdate />} />,
+    <Route path="/notice" element={<ProtectedRoute element={<NoticeList />} />} />,
+    <Route path="/noticewrite" element={<ProtectedRoute element={<NoticeWrite />} />} />,
+    <Route path="/noticedetail/:notId" element={<ProtectedRoute element={<NoticeDetail />} />} />,
+    <Route path="/noticeupdate/:notId" element={<ProtectedRoute element={<NoticeUpdate />} />} />,
     </>
 
 
