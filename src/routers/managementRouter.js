d59dfc62be772a5120e21310a884a4348dc14e3d
hanadoolyseoi <hanadoolyseoi@gmail.com>
2025-01-09 19:03:55 +0900

    fix : tts 기능 추가 및 managementRouter 수정
    fix : tts 기능 추가 및 managementRouter 수정

--- src/routers/managementRouter.js
+++ src/routers/managementRouter.js
@@ -8,9 +8,9 @@ import ProtectedRoute from '../components/common/ProtectedRoute';
 
 const managementRouter = [
   <>
-    <Route path="/seniorlist" element={<SeniorList />} />
-    <Route path="/seniorlist/sdetailview/:UUID" element={<SeniorDetailView />} />
-    <Route path="/seniorlist/seniorregist" element={<SeniorRegist />} />
+    <Route path="/seniorlist" element={<ProtectedRoute element={<SeniorList />} />} />,
+    <Route path="/seniorlist/sdetailview/:UUID" element={<ProtectedRoute element={<SeniorDetailView />} />} />,
+    <Route path="/seniorlist/seniorregist" element={<ProtectedRoute element={<SeniorRegist />} />} />,
   </>,
 ];
 
