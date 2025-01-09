2180e28ea708c38809dfff16ec498a5039181cb7
SangMoo Lee <dltkdan7373@hanmail.net>
2025-01-09 14:43:31 +0900

    feat 사이드바 수정

--- src/components/common/SideBar.js
+++ src/components/common/SideBar.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import styles from './SideBar.module.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../../AuthProvider';
 
 function SideBar() {
@@ -16,172 +16,72 @@ function SideBar() {
     }
   };
 
-  if (role === 'ADMIN') {
-    return (
-      <header className={styles.sideBarHeader}>
-        <Link to="/" className={styles.sideTitle}>
-          실버플러스
-        </Link>
-        <ul className={styles.sideBar}>
-          <li>
-            <Link to="/">검색</Link>
-          </li>
-          <li>
-            <Link to="/mlistview">계정관리</Link>
-          </li>
-          <li>
-            <Link to="/notice">공지사항</Link>
-          </li>
-          <li>
-            <Link to="/program">어르신 프로그램</Link>
-          </li>
-          <li>
-            <Link to="/qna">Q&A</Link>
-          </li>
-          <li>
-            <Link to="/myinfoadmin">마이페이지</Link>
-          </li>
-        </ul>
-        <ul className={styles.logout}>
-          {isLoggedIn ? (
-            <li>
-              <button
-                style={{
-                  backgroundColor: '#064420',
-                  color: '#ffffff',
-                  fontSize: '28px',
-                  border: 'none',
-                  boxShadow: 'none',
-                  outline: 'none',
-                  cursor: 'pointer',
-                }}
-                onClick={handleLogout}
-              >
-                로그아웃
-              </button>
-            </li>
-          ) : (
-            <li>
-              <Link to="/loginmember">로그인</Link>
-            </li>
-          )}
-        </ul>
-      </header>
-    );
-  }
+  const renderLinks = (links) =>
+    links.map(({ path, label,img }) => (
+      <li key={path}>
+        <NavLink
+          to={path}
+          className={({ isActive }) =>
+            isActive ? `${styles.activeLink}` : ''
+          } // 활성화된 링크에 클래스 추가
+        >
+          {img} &nbsp; {label}
+        </NavLink>
+      </li>
+    ));
 
-  if (role === 'MANAGER') {
-    return (
-      <header className={styles.sideBarHeader}>
-        <Link to="/" className={styles.sideTitle}>
-          실버플러스
-        </Link>
-        <ul className={styles.sideBar}>
-          <li>
-            <Link to="/">검색</Link>
-          </li>
-          <li>
-            <Link to="/dashlist">대시보드</Link>
-          </li>
-          <li>
-            <Link to="/seniorlist">어르신관리</Link>
-          </li>
-          <li>
-            <Link to="/notice">공지사항</Link>
-          </li>
-          <li>
-            <Link to="/program">어르신 프로그램</Link>
-          </li>
-          <li>
-            <Link to="/book">전자책</Link>
-          </li>
-          <li>
-            <Link to="/qna">Q&A</Link>
-          </li>
-          <li>
-            <Link to="/myinfomanager">마이페이지</Link>
-          </li>
-        </ul>
-        <ul className={styles.logout}>
-          {isLoggedIn ? (
-            <li>
-              <button
-                style={{
-                  backgroundColor: '#064420',
-                  color: '#ffffff',
-                  fontSize: '28px',
-                  border: 'none',
-                  boxShadow: 'none',
-                  outline: 'none',
-                  cursor: 'pointer',
-                }}
-                onClick={handleLogout}
-              >
-                로그아웃
-              </button>
-            </li>
-          ) : (
-            <li>
-              <Link to="/loginmember">로그인</Link>
-            </li>
-          )}
-        </ul>
-      </header>
-    );
-  }
+  const adminLinks = [
+    { path: '/', label: '검색', img : <span class="material-symbols-outlined">search</span> },
+    { path: '/mlistview', label: '계정관리', img : <span class="material-symbols-outlined">manage_accounts</span>  },
+    { path: '/notice', label: '공지사항', img : <span class="material-symbols-outlined">description</span>  },
+    { path: '/program', label: '어르신 프로그램', img : <span class="material-symbols-outlined">diversity_1</span>  },
+    { path: '/qna', label: 'Q&A', img : <span class="material-symbols-outlined">help_center</span>  },
+    { path: '/myinfoadmin', label: '마이페이지', img : <span class="material-symbols-outlined">account_circle</span>  },
+  ];
 
-  if (role === 'FAMILY') {
-    return (
-      <header className={styles.sideBarHeader}>
-        <Link to="/" className={styles.sideTitle}>
-          실버플러스
-        </Link>
-        <ul className={styles.sideBar}>
+  const managerLinks = [
+    { path: '/', label: '검색', img : <span class="material-symbols-outlined">search</span> },
+    { path: '/dashlist', label: '대시보드', img : <span class="material-symbols-outlined">home</span> },
+    { path: '/seniorlist', label: '어르신관리', img : <span class="material-symbols-outlined">manage_accounts</span> },
+    { path: '/notice', label: '공지사항', img : <span class="material-symbols-outlined">description</span> },
+    { path: '/program', label: '어르신 프로그램', img : <span class="material-symbols-outlined">diversity_1</span> },
+    { path: '/book', label: '전자책', img : <span class="material-symbols-outlined">menu_book</span> },
+    { path: '/qna', label: 'Q&A', img : <span class="material-symbols-outlined">help_center</span> },
+    { path: '/myinfomanager', label: '마이페이지', img : <span class="material-symbols-outlined">account_circle</span> },
+  ];
+
+  const familyLinks = [
+    { path: '/', label: '검색', img : <span class="material-symbols-outlined">search</span> },
+    { path: '/seniorlist', label: '어르신관리', img : <span class="material-symbols-outlined">manage_accounts</span> },
+    { path: '/notice', label: '공지사항', img : <span class="material-symbols-outlined">description</span> },
+    { path: '/program', label: '어르신 프로그램', img : <span class="material-symbols-outlined">diversity_1</span> },
+    { path: '/qna', label: 'Q&A', img : <span class="material-symbols-outlined">help_center</span> },
+    { path: '/myinfofamily', label: '마이페이지', img : <span class="material-symbols-outlined">account_circle</span> },
+  ];
+
+  return (
+    <header className={styles.sideBarHeader}>
+      <NavLink to="/" className={styles.sideTitle}>
+        실버플러스
+      </NavLink>
+      <ul className={styles.sideBar}>
+        {role === 'ADMIN' && renderLinks(adminLinks)}
+        {role === 'MANAGER' && renderLinks(managerLinks)}
+        {role === 'FAMILY' && renderLinks(familyLinks)}
+      </ul>
+      <ul className={styles.logout}>
+        {isLoggedIn ? (
           <li>
-            <Link to="/">검색</Link>
+            <button onClick={handleLogout}><span class="material-symbols-outlined">logout</span> &nbsp; 로그아웃</button>
           </li>
+        ) : (
           <li>
-            <Link to="/seniorlist">어르신관리</Link>
+            <NavLink to="/loginmember">로그인</NavLink>
           </li>
-          <li>
-            <Link to="/notice">공지사항</Link>
-          </li>
-          <li>
-            <Link to="/program">어르신 프로그램</Link>
-          </li>
-          <li>
-            <Link to="/qna">Q&A</Link>
-          </li>
-          <li>
-            <Link to="/myinfofamily">마이페이지</Link>
-          </li>
-        </ul>
-        <ul className={styles.logout}>
-          {isLoggedIn ? (
-            <li>
-              <button
-                style={{
-                  backgroundColor: '#064420',
-                  color: '#ffffff',
-                  fontSize: '28px',
-                  border: 'none',
-                  boxShadow: 'none',
-                  outline: 'none',
-                  cursor: 'pointer',
-                }}
-                onClick={handleLogout}
-              >
-                로그아웃
-              </button>
-            </li>
-          ) : (
-            <li>
-              <Link to="/loginmember">로그인</Link>
-            </li>
-          )}
-        </ul>
-      </header>
-    );
-  }
+        )}
+      </ul>
+    </header>
+  );
 }
+
 export default SideBar;
