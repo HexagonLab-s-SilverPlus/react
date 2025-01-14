fa20d1f7533bb21fc6c66ed7bb2e5f01725d1813
SangMoo Lee <dltkdan7373@hanmail.net>
2025-01-14 23:09:04 +0900

    fix 게임 시작페이지 수정

--- src/pages/game/ReadyGame.js
+++ src/pages/game/ReadyGame.js
@@ -6,6 +6,10 @@ import { useNavigate } from 'react-router-dom';
 import {AuthContext} from '../../AuthProvider';
 // components
 import SeniorNavbar from '../../components/common/SeniorNavbar';
+//image
+import info1 from '../../assets/images/game/info1.png';
+import info2 from '../../assets/images/game/info2.png';
+import info3 from '../../assets/images/game/info3.png';
 
 
 const ReadyGame = () => {
@@ -22,6 +26,11 @@ const ReadyGame = () => {
       <div className={styles.gameWelcome}>
         <p>
           {member.memName}님 카드맞추기 게임에 오신것을 환영합니다.<br/>
+          <div className={styles.gameImgWrap}>
+            <div className={styles.gameImgBox}><img className={styles.gameInfoImg} src={info1} /></div>
+            <div className={styles.gameImgBox}><img className={styles.gameInfoImg} src={info2}/></div>
+            <div className={styles.gameImgBox}><img className={styles.gameInfoImg} src={info3}/></div>
+          </div>
           게임시작 버튼을 눌러 게임을 시작해 보세요.😊
         </p>
         <button onClick={()=>(nav('/game/play'))}>게임시작</button> &nbsp;
